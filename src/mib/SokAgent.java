1c3f65ac6ef20f8d1a3f0dcf71839ee60b1ff62b
Lunawish <96469986+Lunawish@users.noreply.github.com>
2022-01-03 15:47:17 +0100

    lililili

--- src/mib/SokAgent.java
+++ src/mib/SokAgent.java
@@ -15,12 +15,16 @@ import oru.inf.InfException;
 public class SokAgent extends javax.swing.JFrame {
 
     private InfDB idb;
+    private String anvId;
+    private String anvTyp;
     /**
      * Creates new form SokAgent
      */
-    public SokAgent() {
+    public SokAgent(InfDB idb, String anvId, String anvTyp) {
         initComponents();
         this.idb = idb;
+        this.anvId = anvId;
+        this.anvTyp = anvTyp;
     }
 
     /**
@@ -171,8 +175,10 @@ public class SokAgent extends javax.swing.JFrame {
     String soktID = txtSoktID.getText();
     
     String agentNamn = idb.fetchSingle("SELECT Namn FROM Agent WHERE Agent_ID = " + soktID +"");
+    String agentTelefon = idb.fetchSingle("SELECT Telefon FROM Agent WHERE Agent_ID = " + soktID +"");
     
     lblBenamningSvar.setText(agentNamn);
+    
     
     }
     
@@ -186,36 +192,7 @@ public class SokAgent extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SokAgent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SokAgent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SokAgent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SokAgent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SokAgent().setVisible(true);
-            }
-        });
+   
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
