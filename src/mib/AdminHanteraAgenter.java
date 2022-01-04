7e41db2793a008be286bdb0f78c025c7ca0b6f8a
Lunawish <96469986+Lunawish@users.noreply.github.com>
2022-01-04 13:14:43 +0100

    blandat?

--- src/mib/AdminHanteraAgenter.java
+++ src/mib/AdminHanteraAgenter.java
@@ -109,7 +109,9 @@ public class AdminHanteraAgenter extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        setVisible(false);
+        RegistreraAgent registreraAgent = new RegistreraAgent (idb, anvId, anvTyp);
+        registreraAgent.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
