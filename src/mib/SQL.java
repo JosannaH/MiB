d66c63de198c7dd2cb6f15816b867bf50f3b5a13
Josanna <josanna@live.se>
2022-01-07 14:49:16 +0100

    SQL klar

--- src/mib/SQL.java
+++ src/mib/SQL.java
@@ -170,13 +170,18 @@ public class SQL extends javax.swing.JFrame {
             System.out.println("Internt felmeddelande:" + e.getMessage());
         }
     }
-    
+    /**
+     * Kolla om agenten är ansvarig över någon alien
+     * @param soktID
+     * @param idb
+     * @return 
+     */
     public static boolean agentHarAlien(String soktID, InfDB idb){
         boolean resultat = true;
         String count = "";
         
         try{
-              // Kolla om agenten är ansvarig för några Aliens
+              // Kolla hur många aliens en agent är ansvarig över
                count = idb.fetchSingle("SELECT count(alien_ID) FROM alien WHERE ansvarig_agent = " + soktID);
         }
             catch (InfException e) {
@@ -188,7 +193,6 @@ public class SQL extends javax.swing.JFrame {
             resultat = false;
         }
         return resultat;
-        
     }
 
     /**
@@ -201,19 +205,21 @@ public class SQL extends javax.swing.JFrame {
     public void getAliensForAnsvaigAgent(String agent_ID, JTextArea txtAreaAliens) {
         // Töm textarea inför ny sökning
         txtAreaAliens.setText("");
+        // Skriv ut rubrik i textares
         txtAreaAliens.append("Alien ID \t Namn \n");
 
         ArrayList<HashMap<String, String>> hashID = new ArrayList<>();
         ArrayList<HashMap<String, String>> hashNamn = new ArrayList<>();
 
         try {
+            // Hämta namn och ID för de aliens som har den specifika agenten som ansvarig
             hashID = idb.fetchRows("SELECT alien_ID FROM alien WHERE ansvarig_agent = " + agent_ID + " ORDER BY alien_ID ASC");
             hashNamn = idb.fetchRows("SELECT namn FROM alien WHERE ansvarig_agent = " + agent_ID + " ORDER BY alien_ID ASC");
         } catch (InfException e) {
             JOptionPane.showMessageDialog(null, "Något gick fel!");
             System.out.println("Internt felmeddelande: getAliensForAnsvarigAgent() " + e.getMessage());
         }
-
+        // Loopa igenom lista och skriv ut info i textarea
         for (int i = 0; i < hashID.size(); i++) {
             txtAreaAliens.append(hashID.get(i).get("alien_ID") + "\t" + hashNamn.get(i).get("namn") + "\n");
         }
@@ -229,21 +235,28 @@ public class SQL extends javax.swing.JFrame {
      * @param ordning
      */
     public void getRegistreringsdatum(String franDatum, String tillDatum, JTextArea txtAreaListaDatum, String ordning) {
-
+        // Tar in användarinmatningen i en array. Användaren matar in ÅÅÅÅ-MM-DD, här splittas det upp så år, månad och dag hamnar
+        // på varsin indexplats
         String[] fran = franDatum.split("-");
         String[] till = tillDatum.split("-");
+        // Variabler att spara datumDB i
         String franDatumString = "";
         String tillDatumString = "";
 
+        // datumDB behöver göras om till int för att kunna jämföras större än/mindre än
         int franDatumInt = 0;
         int tillDatumInt = 0;
 
         try {
+            // Sätt ihop datumet till rätt format, dvs ÅÅÅÅMMDD och spara i en String
             franDatumString = fran[0] + fran[1] + fran[2];
             tillDatumString = till[0] + till[1] + till[2];
 
             franDatumInt = Integer.parseInt(franDatumString);
             tillDatumInt = Integer.parseInt(tillDatumString);
+            
+            // Fånga upp tex indexOUtOfBounds pga felinmatning från användaren, eller om användaren inte skrivit siffror och 
+            // då inte går att göra parseInt
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Något gick fel! Kontrollera att datumen är korrekt ifyllda: ÅÅÅÅ-MM-DD");
         }
@@ -260,7 +273,8 @@ public class SQL extends javax.swing.JFrame {
         String queryDatum;
         String queryID;
         String queryNamn;
-
+        
+        // Olika frågor ska ställas till databasen beroende på i vilken ordning användaren vill lista aliens
         if (ordning.equals("Äldsta först")) {
             queryDatum = "SELECT date_format(Registreringsdatum, '%Y%m%d') FROM alien ORDER BY Registreringsdatum ASC";
             queryID = "SELECT alien_ID FROM alien ORDER BY Registreringsdatum ASC";
@@ -272,25 +286,29 @@ public class SQL extends javax.swing.JFrame {
         }
 
         try {
+            // Hämta all data från tabellen alien och lägg in i listor
+            // Databasfrågorna beror på if-satsen ovan, i vilken ordning som användaren vill visa datan
             listaDatum = idb.fetchColumn(queryDatum);
             listaID = idb.fetchColumn(queryID);
             listaNamn = idb.fetchColumn(queryNamn);
-
+            
+            // Loopa igenom datumlistan och skriv ut info i textarea för de datumDB som håller sig inom önskat spann
             for (int i = 0; i < listaDatum.size(); i++) {
+                // Datum från databasen som ska jämföras med datumspann som användaren angett
+                String datumDB = listaDatum.get(i);
+                int datumDBInt = Integer.parseInt(datumDB);
 
-                String datum = listaDatum.get(i);
-                int datumInt = Integer.parseInt(datum);
-
-                if (datumInt >= franDatumInt && datumInt <= tillDatumInt) {
+                // om datumet är inom spannet så skrivs aliens info ut i textarea
+                if (datumDBInt >= franDatumInt && datumDBInt <= tillDatumInt) {
                     String id = listaID.get(i);
                     String namn = listaNamn.get(i);
 
-                    txtAreaListaDatum.append(datum + "\t" + id + "\t" + namn + "\n");
+                    txtAreaListaDatum.append(datumDB + "\t" + id + "\t" + namn + "\n");
                 }
             }
         } catch (InfException e) {
             JOptionPane.showMessageDialog(null, "Något gick fel! Kontrollera att datumen är korrekt ifyllda: ÅÅÅÅ-MM-DD");
-            System.out.println("Internt felmeddelande: getPlatser() " + e.getMessage());
+            System.out.println("Internt felmeddelande: " + e.getMessage());
         }
     }
 
@@ -302,8 +320,11 @@ public class SQL extends javax.swing.JFrame {
      */
     public void getOmradeschef(JLabel lblChef, String omrade) {
         try {
+            // Hämta ID för området som användaren valt
             String omradesID = idb.fetchSingle("SELECT Omrades_ID FROM omrade WHERE Benamning = '" + omrade + "'");
+            // Hämta ID för den agent som är chef över område
             String agentID = idb.fetchSingle("SELECT Agent_ID FROM omradeschef WHERE Omrade =" + omradesID);
+            // Utifrån agent_ID hämtas namnet på agenten för att kunna presentera den för användaren
             String agentNamn = idb.fetchSingle("SELECT Namn FROM agent WHERE Agent_ID =" + agentID);
 
             lblChef.setText("Områdeschef för " + omrade + " är " + agentNamn + "");
@@ -345,6 +366,7 @@ public class SQL extends javax.swing.JFrame {
         String chefID = "";
         String chefNamn = "";
         try {
+            // Hämta först agent_ID för chefen och utifrån det hämtas sedan namnet på agenten
             chefID = idb.fetchSingle("SELECT agent_ID FROM kontorschef WHERE kontorsbeteckning = '" + kontor + "'");
             chefNamn = idb.fetchSingle("SELECT namn FROM agent WHERE agent_ID = " + chefID);
         } catch (InfException e) {
@@ -369,15 +391,18 @@ public class SQL extends javax.swing.JFrame {
         ArrayList<String> omradesLista = new ArrayList();
 
         try {
-            // Från databasen hämtas information om chefsID och områdesID.
+            //Hämta agentens ID beserat på vilken agent användaren alt i comboboxen
             nyChefID = idb.fetchSingle("SELECT agent_ID FROM agent WHERE namn = '" + nyChefNamn + "'");
+            // Hämta områdesID utifrån vilket område användaren valt
             omradeID = idb.fetchSingle("SELECT omrades_ID FROM omrade WHERE benamning = '" + omrade + "'");
 
-            // Listorna fylls med de ID som finns i databasen.
+            // Hämta ID för alla områdeschefer
             omradesChefsLista = idb.fetchColumn("SELECT agent_ID FROM omradeschef");
+            // Hämta alla områden som har en chef
             omradesLista = idb.fetchColumn("SELECT omrade FROM omradeschef");
 
-            // Då en agent endast kan vara chef för ett område kontrollerar man för detta nedan.
+            // Om den som användaren vill sätta som ny chef redan är chef för något område, dvs finns med i omradesChefsLista
+            // Så visas ett meddelande att hen redan är områdeschef. Annars körs följande kod
             if (!omradesChefsLista.contains(nyChefID)) {
 
                 // Om ett område finns med i tabellen uppdateras den, om inte så lägger man till denna information.
@@ -411,22 +436,22 @@ public class SQL extends javax.swing.JFrame {
     public void uppdateraKontorschef(String nyChefNamn, String kontor, JLabel lblNyChef) {
         String nyChefID = "";
 
-        // Två listor skapas för att kunna leta upp relevant agentID och benämning.
+        // Två listor skapas för att kunna leta upp relevant agentID och kontorsbenämning.
         ArrayList<String> chefsLista = new ArrayList<>();
         ArrayList<String> kontorsLista = new ArrayList();
 
         try {
-            // Från databasen hämtas information om chefsID.
+            // Från databasen hämtas information om agentID för vald agent
             nyChefID = idb.fetchSingle("SELECT agent_ID FROM agent WHERE namn = '" + nyChefNamn + "'");
 
-            // Listorna fylls med de ID  och kontorsnamn som finns i databasen.
+            // Listorna fylls med alla ID  och kontorsnamn som finns i databasen.
             chefsLista = idb.fetchColumn("SELECT agent_ID FROM kontorschef");
             kontorsLista = idb.fetchColumn("SELECT kontorsbeteckning FROM kontorschef");
 
-            // Kontrollerar om agenten redan är kontorschef.
+            // Kontrollerar om agenten redan är kontorschef, dvs den får inte finns med i chefslistan
             if (!chefsLista.contains(nyChefID)) {
 
-                // Om ett område finns med i tabellen uppdateras den, om inte så lägger man till denna information.
+                // Om ett kontor finns med i tabellen (dvs har en nyvarnade chef) uppdateras den, om inte så lägger man till denna information.
                 if (kontorsLista.contains(kontor)) {
                     idb.update("UPDATE kontorschef SET agent_ID = " + nyChefID + " WHERE kontorsbeteckning = '" + kontor + "'");
                     // Visar användaren vem som är ny chef. 
@@ -442,12 +467,17 @@ public class SQL extends javax.swing.JFrame {
 
         } catch (InfException e) {
             JOptionPane.showMessageDialog(null, "Något gick fel!");
-            System.out.println("Internt felmeddelande: uppdateraOmradeschef() " + e.getMessage());
+            System.out.println("Internt felmeddelande: uppdateraKontorschef() " + e.getMessage());
         }
         // Visar användaren vem som är ny chef. 
         lblNyChef.setText("Ny chef för " + kontor + " är " + nyChefNamn);
     }
 
+    /**
+     * Kollar om en agent är administratör
+     * @param anvID
+     * @return 
+     */
     public boolean arAdmin(String anvID) {
         boolean result = false;
         String admin = "";
@@ -461,12 +491,17 @@ public class SQL extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Något gick fel!");
             System.out.println("Internt felmeddelande: arAdmin() " + e.getMessage());
         }
-
         return result;
     }
 
+    /**
+     * Skickar tillbaka användaren till startsida, olika sidor beroende på användartyp
+     * @param anvId
+     * @param anvTyp 
+     */
     public void tillStartsida(String anvId, String anvTyp) {
         setVisible(false);
+        // anropar metod för att kolla om agenten är admin
         boolean arAdmin = arAdmin(anvId);
 
         if (anvTyp.equals("Alien")) {
@@ -480,6 +515,7 @@ public class SQL extends javax.swing.JFrame {
             a.setVisible(true);
         }
     }
+    
     /**
  * Kontrollerar så att ett agentnamn inte redan finns i tabellen agent
  * @param rutaAttKolla
@@ -512,11 +548,14 @@ public class SQL extends javax.swing.JFrame {
  */
     public boolean namnFinnsInteAlien(JTextField rutaAttKolla) {
         boolean resultat = true;
+        //Hämtar namn som användaren angett
         String namn = rutaAttKolla.getText();
 
         try {
+            // Spara alla aliennamn i lista
             ArrayList<String> namnLista = idb.fetchColumn("SELECT namn FROM alien");
             if (namnLista.contains(namn)) {
+                // om listan innehåller namnet returneras false
                 resultat = false;
                 JOptionPane.showMessageDialog(null, "Namnet på alien finns redan!");
             }
@@ -526,5 +565,4 @@ public class SQL extends javax.swing.JFrame {
         }
         return resultat;
     }
-
 }
